fd88364a3249a49346bd1d61db2bbec75d14e9e5
leenabaxioned <leenab@axioned.com>
2024-06-27 13:27:53 +0530

    script added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
 /* Author: */
+var scrollUp = document.querySelector(".scroll");
+scrollUp.addEventListener("click", scrollToTop);
+function scrollToTop() {
+    $(window).scrollTop(0);
+}
+
+window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("header").classList.add("scrolled");
+    } else {
+      document.getElementById("header").classList.remove("scrolled");
+    }
+  }
